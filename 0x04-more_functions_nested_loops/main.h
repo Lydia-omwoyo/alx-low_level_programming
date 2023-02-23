8c505c76639dc1ecc39a45fbb24b576543d3d2cc
Lydia-omwoyo <lydiaomwoyo10@gmail.com>
2023-02-23 07:57:56 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -12,6 +11,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int _putchar(char);
 void print_number(int n);
-#endif
 
+#endif
