918cf1b29f6643e7b6982de5471d4cb69cc5397d
Lydia-omwoyo <lydiaomwoyo10@gmail.com>
2023-03-09 23:24:45 -0800

    files

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is very fun");
+	return (0);
+}
+
